29c8a8918a7b7513e768e88dae8a6be971f78c46
sian2005 <62208362+sian2005@users.noreply.github.com>
2024-03-20 00:23:21 +0100

    Extracted method from initializeUI in MaasMapsUI
    Extracted buildTestMap() to make the line and markers from the test environment by Alexandra

--- src/ui/map/MaasMapsUI.java
+++ src/ui/map/MaasMapsUI.java
@@ -22,14 +22,18 @@ public class MaasMapsUI extends JFrame {
 
         add(map = new Map());
 
+        buildTestMap();
+
+        revalidate();
+    }
+
+    private void buildTestMap() {
         map.addLine(new Line(
-            new Location(50.853617, 5.692009), new Location(50.853037,5.691825),
-            new Location(50.90074,5.714544), new Location(50.877296,5.672557)
+            new Location(50.853037, 5.691825),
+            new Location(50.90074, 5.714544), new Location(50.877296 ,5.672557)
         ));
 
         map.addMarker(new Marker(new Location(50.853617, 5.692009)));
-
-        revalidate();
     }
 }
 
