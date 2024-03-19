2a603f0b4f32a2d6b870c70977bff0df1f97b652
alepskn <alexandraplishkin@gmail.com>
2024-03-19 13:45:16 +0100

    changed to return a point

--- src/fileaccess/Converter.java
+++ src/fileaccess/Converter.java
@@ -1,16 +1,17 @@
 package fileaccess;
 
 import models.Location;
+import java.awt.Point;
 
 public class Converter {
     private static final double MIN_LONGITUDE_MAASTRICHT = 5.64213;
     private static final double MAX_LATITUDE_MAASTRICHT = 50.90074;
     private static final int SCALE = 10;
 
-    public static Location convertedLocation(Location location) {
-        double latitude = ((MAX_LATITUDE_MAASTRICHT - location.getLatitude()) * SCALE);
-        double longitude = ((location.getLongitude() - MIN_LONGITUDE_MAASTRICHT) * SCALE);
+    public static Point convertedLocation(Location location) {
+        int latitude = (int) ((MAX_LATITUDE_MAASTRICHT - location.getLatitude()) * SCALE);
+        int longitude = (int) ((location.getLongitude() - MIN_LONGITUDE_MAASTRICHT) * SCALE);
 
-        return new Location(latitude, longitude);
+        return new Point(latitude, longitude);
     }
 }
