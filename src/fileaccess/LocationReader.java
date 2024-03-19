1969aaf7b7ee5158aebbf06f788303052ab7cefa
Sian Lodde <62208362+sian2005@users.noreply.github.com>
2024-03-19 13:11:37 +0100

    Added getters and setters in map class

--- src/fileaccess/LocationReader.java
+++ src/fileaccess/LocationReader.java
@@ -1,4 +1,5 @@
 package fileaccess;
+
 import models.Location;
 
 public interface LocationReader {
--- src/fileaccess/ZipCodeCSVParser.java
+++ src/fileaccess/ZipCodeCSVParser.java
@@ -1,4 +1,5 @@
 package fileaccess;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
