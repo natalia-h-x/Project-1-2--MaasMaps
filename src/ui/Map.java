d810138b619240190179cda10b97aaa6b789fe1f
Sian Lodde <62208362+sian2005@users.noreply.github.com>
2024-03-19 11:32:51 +0100

    Implement Moveable interface
    - Containing two methods for manipulating any movable class

--- src/ui/Map.java
+++ src/ui/Map.java
@@ -7,46 +7,58 @@ import java.io.*;
 
 import javax.swing.*;
 
-public class Map extends JPanel{
-    public BufferedImage map;
-    public int mapWidth;
-    public int mapHeight;
-    public double scale;
+public class Map extends JPanel implements Moveable {
+    private transient BufferedImage mapImage;
+    private transient int mapWidth;
+    private transient int mapHeight;
+    private double scale;
     private Point offset;
 
-    public Map(){
+    public Map() {
         scale = 1;
-        offset = new Point(0,0);
+        offset = new Point(0, 0);
 
         try {
-            BufferedImage mapImage = MapManager.getMapData();
-            loadMap(mapImage);
-        } catch (IOException e) {
+            loadMap(MapManager.getMapData());
+        }
+        catch (IOException e) {
             e.printStackTrace();
         }
-
-        repaint();
     }
 
     @Override
     public void paint(Graphics g) {
         super.paint(g);
 
-        if (map == null)
+        if (mapImage == null)
             return;
 
         Graphics2D g2 = (Graphics2D) g;
 
-        TexturePaint paint = new TexturePaint(map, new Rectangle2D.Double(offset.x, offset.y, mapWidth * scale, mapHeight * scale));
+        TexturePaint paint = new TexturePaint(mapImage, new Rectangle2D.Double(offset.x, offset.y, mapWidth * scale, mapHeight * scale));
         g2.setPaint(paint);
         g2.fill(new Rectangle2D.Double(offset.x, offset.y, mapWidth * scale, mapHeight * scale));
     }
 
-    private void loadMap(BufferedImage mapImage){
-        map = mapImage;
+    private void loadMap(BufferedImage image){
+        mapImage = image;
 
-        mapWidth = map.getWidth();
-        mapHeight = map.getHeight();
+        mapWidth = image.getWidth();
+        mapHeight = image.getHeight();
+
+        repaint();
+    }
+
+    @Override
+    public void setScale(int scale) {
+        this.scale = scale;
+
+        repaint();
+    }
+
+    @Override
+    public void setTranslation(Point offset) {
+        this.offset = offset;
 
         repaint();
     }
---
+++ src/ui/Moveable.java
@@ -0,0 +1,8 @@
+package ui;
+
+import java.awt.Point;
+
+public interface Moveable {
+    public abstract void setScale(int scale);
+    public abstract void setTranslation(Point point);
+}
