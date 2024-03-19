1c6184fd9daf9cd59710d299444f0b6dec083baf
alepskn <alexandraplishkin@gmail.com>
2024-03-19 13:27:50 +0100

    e

--- src/database/Converter.java
+++ src/database/Converter.java
@@ -3,14 +3,14 @@ package database;
 import models.Location;
 
 public class Converter {
-    private static final double LONGITUDE_MAASTRICHT = 5.64213;
-    private static final double LATITUDE_MAASTRICHT = 50.90074;
+    private static final double MIN_LONGITUDE_MAASTRICHT = 5.64213;
+    private static final double MAX_LATITUDE_MAASTRICHT = 50.90074;
     private static final int SCALE = 100000;
 
     public static Location convertedLocation(Location location) {
         
-        double latitude =  - ((location.getLatitude() - LATITUDE_MAASTRICHT) * SCALE);
-        double longitude =  ((location.getLongitude() - LONGITUDE_MAASTRICHT) * SCALE);
+        double latitude =  ((MAX_LATITUDE_MAASTRICHT - location.getLatitude()) * SCALE);
+        double longitude =  ((location.getLongitude() - MIN_LONGITUDE_MAASTRICHT) * SCALE);
 
         return new Location(latitude, longitude);
     }
