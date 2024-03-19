158317de6effea1ee1898d1d6cf0495422df3b19
alepskn <alexandraplishkin@gmail.com>
2024-03-19 13:38:22 +0100

    a

--- src/fileaccess/Converter.java
+++ src/fileaccess/Converter.java
@@ -5,10 +5,9 @@ import models.Location;
 public class Converter {
     private static final double MIN_LONGITUDE_MAASTRICHT = 5.64213;
     private static final double MAX_LATITUDE_MAASTRICHT = 50.90074;
-    private static final int SCALE = 100000;
+    private static final int SCALE = 10;
 
     public static Location convertedLocation(Location location) {
-        
         double latitude =  ((MAX_LATITUDE_MAASTRICHT - location.getLatitude()) * SCALE);
         double longitude =  ((location.getLongitude() - MIN_LONGITUDE_MAASTRICHT) * SCALE);
 
