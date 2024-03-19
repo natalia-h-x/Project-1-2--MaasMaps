c738f937bf32ac4dab455f14986ed9b1c060cbbb
Sian Lodde <62208362+sian2005@users.noreply.github.com>
2024-03-19 11:41:44 +0100

    Restructure of UI Classes and formatting
    - Formatted MaasMaps and Map
    - Added packages for UI managers and UI interfaces

---
+++ src/ui/map/MaasMapsUI.java
@@ -0,0 +1,18 @@
+package ui.map;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class MaasMapsUI extends JFrame {
+    public MaasMapsUI(){
+        super("Maas Maps");
+        initialiseUI();
+    }
+
+    private void initialiseUI(){
+        setSize(500, 300);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+        add(new Map());
+    }
+}
