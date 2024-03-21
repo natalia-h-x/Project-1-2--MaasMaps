4796eac82e540a4b0b2dde2104dab0ed3ea935a2
Sian Lodde & Natalia <62208362+sian2005@users.noreply.github.com>
2024-03-21 15:18:40 +0100

    Added javadoc to context class

--- src/core/Context.java
+++ src/core/Context.java
@@ -2,6 +2,12 @@ package core;
 
 import ui.map.ProxyMap;
 
+/**
+ * This class represents the Context singleton that harbour all share objets amoungst classes.
+ *
+ * @author Sian Lodde
+ * @author Alexandra
+ */
 public class Context {
     private static Context context;
     private ProxyMap proxyMap;
