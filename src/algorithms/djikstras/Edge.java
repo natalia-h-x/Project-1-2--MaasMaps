b2da3ceb401d697fa67e609ef213d60b6b47aeb2
Navridis, Kimon (Stud. DACS) <k.navridis@student.maastrichtuniversity.nl>
2024-04-11 02:24:02 +0000

    comment

--- src/algorithms/djikstras/Edge.java
+++ src/algorithms/djikstras/Edge.java
@@ -1,6 +1,8 @@
 package algorithms.djikstras;
 import models.Location;
 
+// You can rename this to Route.java and move it to /models
+// can also rename <double weight> to <double distance/distanceInMeters> for intuition
 
 class Edge {
     Location destination;
