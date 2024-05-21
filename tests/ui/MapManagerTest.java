63ad54b6a631e83202414a49105c4b5e7884909e
Sian Lodde <sianlodde@gmail.com>
2024-05-21 14:33:05 +0200

    Fixed MapManagerTest

--- tests/ui/MapManagerTest.java
+++ tests/ui/MapManagerTest.java
@@ -3,23 +3,20 @@ package ui;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import java.awt.Point;
+
 import org.junit.Test;
 
 import core.managers.MapManager;
-import core.models.Location;
 
 public class MapManagerTest {
-    public static void main(String[] args) throws ClassNotFoundException{
-        test1();
-    }
     @Test
-    public static void test1() throws ClassNotFoundException {
-        new MaasMapsUI();
+    public void test1()  {
         try {
-            assert (MapManager.MAP_TOP_LEFT_GLOBAL_XY.equals(MapManager.locationToPoint(MapManager.MAP_TOP_LEFT_LOCATION)));
-            assert (MapManager.MAP_BOTTOM_RIGHT_GLOBAL_XY.equals(MapManager.locationToPoint(MapManager.MAP_BOTTOM_RIGHT_LOCATION)));
+            new MaasMapsUI();
 
-            assert (MapManager.locationToPoint(new Location(50.853571,5.693554)).equals(MapManager.locationToPoint(new Location(50.853551,5.692933))));
+            assertTrue(new Point(0, 0).equals(MapManager.locationToPoint(MapManager.MAP_TOP_LEFT_LOCATION)));
+            assertTrue(new Point(1318, 1583).equals(MapManager.locationToPoint(MapManager.MAP_BOTTOM_RIGHT_LOCATION)));
         }
         catch (Exception e) {
             fail(e);
