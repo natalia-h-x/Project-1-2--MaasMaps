68bb9565937bf5e7c1351835c5e96c2cd56211c9
alepskn <alexandraplishkin@gmail.com>
2024-05-27 22:57:33 +0200

    timetest

---
+++ tests/models/TimeTest.java
@@ -0,0 +1,107 @@
+package models;
+
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import core.models.Time;
+
+public class TimeTest {
+    @Test
+    public void timeTest() {
+        Time one = Time.of(60);
+        Time two = Time.of(120);
+
+        assertEquals(60, one.toSeconds());
+        assertEquals(120, two.toSeconds());
+        one.update(20);
+        assertNotEquals(60, one.toSeconds());
+        assertEquals(20, one.toSeconds());
+
+        Time m = two.minus(one);
+        assertEquals(100, m.toSeconds());
+
+        assertEquals("1 minute and 40 seconds.", m.toString());
+
+        m.update(m.add(Time.of(20)).toSeconds());
+        assertEquals("2 minutes.", m.toString());
+
+        Time n = m.clone();
+        n.update(3600);
+        assertEquals("1 hour.", n.toString());
+
+        n.update(n.add(3620).toSeconds());
+        assertEquals("2 hours and 20 seconds.", n.toString());
+
+        n.update(7260);
+        assertEquals("2 hours and 1 minute.", n.toString());
+
+        n.update(n.add(60).toSeconds());
+        assertEquals("2 hours and 2 minutes.", n.toString());
+
+        n.update(3600+120);
+        assertEquals("1 hour and 2 minutes.", n.toString());
+
+        n.update(20);
+        assertEquals("20 seconds.", n.toString());
+
+        n.update(2*3600);
+        assertEquals("2 hours.", n.toString());
+
+        n.update(60);
+        assertEquals("1 minute.", n.toString());
+
+        n.update(1);
+        assertEquals("1 second.", n.toString());
+
+        n.update(7261);
+        assertEquals("2 hours, 1 minute and 1 second.", n.toString());
+
+        n.update(7322);
+        assertEquals("2 hours, 2 minutes and 2 seconds.", n.toString());
+
+        n.update(7262);
+        assertEquals("2 hours, 1 minute and 2 seconds.", n.toString());
+
+        n.update(7321);
+        assertEquals("2 hours, 2 minutes and 1 second.", n.toString());
+    }
+
+    @Test
+    public void maxTimeTest() {
+        Time one = Time.of(60);
+        one.setHours(Integer.MAX_VALUE);
+        one.setMinutes(1);
+        one.setSeconds(2);
+
+        assertEquals(Integer.MAX_VALUE, one.toSeconds());
+    }
+
+    @Test
+    public void maxTimeTest2() {
+        Time one = Time.of(60);
+        one.setHours(2);
+        one.setMinutes(1);
+        one.setSeconds(Integer.MAX_VALUE);
+
+        assertEquals(Integer.MAX_VALUE, one.toSeconds());
+    }
+
+    @Test
+    public void maxTimeTest3() {
+        Time one = Time.of(60);
+        one.setHours(1);
+        one.setMinutes(Integer.MAX_VALUE);
+        one.setSeconds(2);
+
+        assertEquals(Integer.MAX_VALUE, one.toSeconds());
+    }
+
+    @Test
+    public void maxTimeTest4() {
+        Time one = Time.of("Infinity");
+
+        assertEquals(Integer.MAX_VALUE, one.toSeconds());
+    }
+}
