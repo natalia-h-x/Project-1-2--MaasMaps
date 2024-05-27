4ffdff0623b1ddf5a5c2d59b01292b04681583eb
natalia-h-x <nataliahadjiso@gmail.com>
2024-05-28 01:13:36 +0200

    Fixed appearance of fields when bus selected

--- src/ui/NavigationPanel.java
+++ src/ui/NavigationPanel.java
@@ -1,26 +1,32 @@
 package ui;
 
+import java.awt.Dimension;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
-import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import java.awt.event.ItemEvent;
 
-import core.Constants;
+
+import com.mysql.cj.Constants;
+
+
+import javax.swing.JCheckBox;
+
 import core.Constants.UIConstants;
+import core.Constants.Map;
 import core.Context;
 import core.managers.ExceptionManager;
 import core.managers.MapManager;
-import core.models.Time;
 import core.models.transport.Biking;
 import core.models.transport.Bus;
 import core.models.transport.TransportMode;
@@ -32,11 +38,6 @@ import core.zipcode.ZipCodeDatabase;
  */
 public class NavigationPanel extends JPanel {
     private JLabel timeLabel;
-    private transient TransportMode[] options = {
-        new Walking(),
-        new Biking(),
-        new Bus()
-    };
 
     public NavigationPanel() {
         initialiseNavigationUI();
@@ -72,39 +73,28 @@ public class NavigationPanel extends JPanel {
         departure.setFont(new Font(UIConstants.GUI_FONT_FAMILY, Font.BOLD, UIConstants.GUI_TEXT_FIELD_FONT_SIZE));
         JTextField departureField = new JTextField();
         departureField.setForeground(UIConstants.GUI_HIGHLIGHT_COLOR);
-        departureField.addActionListener(e -> {
-            for (TransportMode option : options) {
-                if (option instanceof Bus b)
-                    b.setDepartingTime(Time.of(departureField.getText()));
-            }
-        });
 
         // create an empty panel for spacing
         JPanel spacerPanel = new JPanel();
-        spacerPanel.setPreferredSize(new java.awt.Dimension(0, 10));
+        spacerPanel.setPreferredSize(new java.awt.Dimension(0, 10)); 
         spacerPanel.setBackground(UIConstants.GUI_BACKGROUND_COLOR);
 
         // create an empty panel 2 for spacing
         JPanel spacerPanel2 = new JPanel();
-        spacerPanel2.setPreferredSize(new java.awt.Dimension(0, 10));
+        spacerPanel2.setPreferredSize(new java.awt.Dimension(0, 10)); 
         spacerPanel2.setBackground(UIConstants.GUI_BACKGROUND_COLOR);
 
         // create search radius label and field
         JLabel search = new JLabel("Search radius: ");
         search.setFont(new Font(UIConstants.GUI_FONT_FAMILY, Font.BOLD, UIConstants.GUI_TEXT_FIELD_FONT_SIZE));
-        JTextField radiusField = new JTextField(Constants.Map.POSTAL_CODE_MAX_SEARCH_RADIUS);
-        radiusField.setForeground(UIConstants.GUI_HIGHLIGHT_COLOR);
-        radiusField.addActionListener(e -> Integer.parseInt(radiusField.getText()));
+        JTextField radiusField = new JTextField(); 
+        radiusField.setText(String.valueOf(Map.POSTAL_CODE_MAX_SEARCH_RADIUS));
 
         // randomize bus stops button
         JButton busRandom = new JButton("Randomize bus stops");
         busRandom.setPreferredSize(new Dimension(10, 25));
         busRandom.setBackground(UIConstants.GUI_BUTTON_COLOR);
         busRandom.setForeground(Color.WHITE);
-        busRandom.addActionListener(e -> {
-            textField1.setText(MapManager.getRandomPostalCode());
-            textField2.setText(MapManager.getRandomPostalCode());
-        });
 
         // arrange text fields to jpanels
         JPanel panel1 = new JPanel();
@@ -114,15 +104,20 @@ public class NavigationPanel extends JPanel {
 
         panel1.setLayout(new GridLayout(6, 0, 0, UIConstants.GUI_BORDER_SIZE / 2));
         panel2.setLayout(new GridLayout(6, 0, 0, UIConstants.GUI_BORDER_SIZE / 2));
+        departure.setVisible(false);
+        departureField.setVisible(false);
+        search.setVisible(false);
+        radiusField.setVisible(false);
+        busRandom.setVisible(false);
         panel1.add(location1);
         panel1.add(location2);
         panel1.add(departure);
-        panel1.add(spacerPanel);
+        panel1.add(spacerPanel); 
         panel1.add(search);
         panel2.add(textField1);
         panel2.add(textField2);
         panel2.add(departureField);
-        panel2.add(spacerPanel2);
+        panel2.add(spacerPanel2); 
         panel2.add(radiusField);
         panel2.add(busRandom, BorderLayout.CENTER);
 
@@ -141,11 +136,15 @@ public class NavigationPanel extends JPanel {
         // create check boxes
         JCheckBox checkBox = new JCheckBox("Yes");
 
+        checkTransfer.setVisible(false);
+        checkBox.setVisible(false);
+
         // Create Combo Box header
         JLabel transportType = new JLabel("Select means of transport: ");
         transportType.setFont(new Font("Select means of transport: ", Font.BOLD, UIConstants.GUI_INFO_FONT_SIZE));
 
         // create combo box
+        TransportMode[] options = {new Walking(), new Biking(), new Bus()};
         JComboBox<TransportMode> selection = new JComboBox<>(options);
         selection.setBackground(UIConstants.GUI_ACCENT_COLOR);
         selection.setForeground(UIConstants.GUI_HIGHLIGHT_COLOR);
@@ -184,33 +183,57 @@ public class NavigationPanel extends JPanel {
         add(navigationButtons, BorderLayout.NORTH);
         add(bottomPanel, BorderLayout.SOUTH);
 
-        addActionListeners(textField1, textField2, calculate, selection);
+        addActionListeners(textField1, textField2, calculate, selection, departure, departureField, search, radiusField, busRandom, checkTransfer, checkBox );
         addClearActionListener(clearButton);
         addBoxActionListener(checkBox);
     }
 
     private void addActionListeners(JTextField textField1, JTextField textField2, JButton calculate,
-            JComboBox<TransportMode> selection) {
+                                JComboBox<TransportMode> selection, JLabel departure, JTextField departureField, JLabel search, JTextField radiusField, JButton busRandom, JLabel checkTransfer, JCheckBox checkBox) {
 
-        final ZipCodeDatabase db = Context.getContext().getZipCodeDatabase();
+    final ZipCodeDatabase db = Context.getContext().getZipCodeDatabase();
 
-        calculate.addActionListener(e -> {
-            try {
-                TransportMode transportMode = (TransportMode) selection.getSelectedItem();
-                transportMode.dispose();
-                transportMode.setStart(db.getLocation(textField1.getText()));
-                transportMode.setDestination(db.getLocation(textField2.getText()));
+    calculate.addActionListener(e -> {
+        try {
+            TransportMode transportMode = (TransportMode) selection.getSelectedItem();
+            transportMode.dispose();
+            transportMode.setStart(db.getLocation(textField1.getText()));
+            transportMode.setDestination(db.getLocation(textField2.getText()));
 
-                Context.getContext().getMap().clearIcons();
-                Context.getContext().getMap().addMapGraphics(transportMode.getGraphics());
+            Context.getContext().getMap().clearIcons();
+            Context.getContext().getMap().addMapGraphics(transportMode.getGraphics());
 
-                timeLabel.setText(UIConstants.GUI_TIME_LABEL_TEXT + transportMode.getTravelTime().toString());
+            timeLabel.setText(UIConstants.GUI_TIME_LABEL_TEXT + transportMode.getTravelTime().toString());
+        } catch (Exception ex) {
+            ExceptionManager.handle(this, ex);
+        }
+    });
+
+    // Add ItemListener to the selection JComboBox
+    selection.addItemListener(e -> {
+        if (e.getStateChange() == ItemEvent.SELECTED) {
+            TransportMode selectedMode = (TransportMode) selection.getSelectedItem();
+            if (selectedMode instanceof Bus) {
+                // Show components related to bus stops
+                departure.setVisible(true);
+                departureField.setVisible(true);
+                search.setVisible(true);
+                radiusField.setVisible(true);
+                busRandom.setVisible(true);
+                checkTransfer.setVisible(true);
+                checkBox.setVisible(true);
+            } else {
+                // Hide components related to bus stops
+                departure.setVisible(false);
+                departureField.setVisible(false);
+                search.setVisible(false);
+                radiusField.setVisible(false);
+                busRandom.setVisible(false);
             }
-            catch (Exception ex) {
-                ExceptionManager.handle(this, ex);
-            }
-        });
-    }
+        }
+    });
+}
+
 
     private void addClearActionListener(JButton clearButton) {
         clearButton.addActionListener(e -> {
@@ -222,10 +245,13 @@ public class NavigationPanel extends JPanel {
     // Add temporary action listener to the checkbox
     private void addBoxActionListener(JCheckBox checkBox) {
         checkBox.addActionListener(e -> {
-            for (TransportMode option : options) {
-                if (option instanceof Bus b)
-                    b.setAllowTransfers(checkBox.isSelected());
+            if (checkBox.isSelected()) {
+                System.out.println("Checkbox is selected");
+            } else {
+                System.out.println("Checkbox is not selected");
             }
         });
     }
 }
+
+
