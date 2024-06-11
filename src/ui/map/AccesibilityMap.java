437ae951044246c16efe8eaf3c79de399a513632
alepskn <alexandraplishkin@gmail.com>
2024-06-11 15:47:22 +0200

    i'm stuck

---
+++ src/ui/map/AccesibilityMap.java
@@ -0,0 +1,13 @@
+package ui.map;
+
+import java.util.List;
+
+public class AccesibilityMap {
+    public AccesibilityMap(List<String> postalCodes) {
+        
+    }
+
+    public void draw() {
+        throw new UnsupportedOperationException();        
+    }
+}
