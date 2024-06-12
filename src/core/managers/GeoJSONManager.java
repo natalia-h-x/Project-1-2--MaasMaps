b70152d3a03dfd667a66fbd1d24d0da304a6481c
sian2005 <62208362+sian2005@users.noreply.github.com>
2024-06-12 16:45:32 +0200

    Create GeoJSONManager.java

---
+++ src/core/managers/GeoJSONManager.java
@@ -0,0 +1,13 @@
+package core.managers;
+
+public class GeoJSONManager {
+    /**
+     * gets the Accessibility Metric.
+     * 
+     * @param postalCode
+     * @return a accessibility metric ranging from 0 - 1.
+     */
+    public double getAccessibilityMetric(String postalCode) {
+        return Math.random();
+    }
+}
