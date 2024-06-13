b09b4fa5f79fb738614ad6ad6893e8af00e3dfa9
sian2005 <sianlodde@gmail.com>
2024-06-13 10:38:43 +0200

    Rename GeoJSONManager to AmenityManager

--- src/core/managers/AmenityManager.java
+++ src/core/managers/AmenityManager.java
@@ -1,13 +1,18 @@
 package core.managers;
 
-public class GeoJSONManager {
+/**
+ * Class for interacting with the data from Geo JSON files.
+ *
+ * @author Kimon
+ */
+public class AmenityManager {
     /**
      * gets the Accessibility Metric.
-     * 
+     *
      * @param postalCode
      * @return a accessibility metric ranging from 0 - 1.
      */
-    public double getAccessibilityMetric(String postalCode) {
+    public static double getAccessibilityMetric(String postalCode) {
         return Math.random();
     }
 }
