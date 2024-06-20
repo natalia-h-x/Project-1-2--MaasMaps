c765d11415492ae3924d4c6221349f9284c0c3b5
sian2005 <sianlodde@gmail.com>
2024-06-20 13:12:34 +0200

    Added heuristic outline

--- src/core/algorithms/AStarAlgorithm.java
+++ src/core/algorithms/AStarAlgorithm.java
@@ -10,12 +10,14 @@ import core.models.transport.Transport;
 public class AStarAlgorithm extends PathStrategy {
     @Override
     public Transport shortestPath(Graph<Point2D> graph, Point2D source, Point2D end, Time startTime) throws IllegalArgumentException {
-        return new DijkstraAlgorithm().shortestPath(graph, source, end, startTime, getHeuristicComparator());
+        return new DijkstraAlgorithm().shortestPath(graph, source, end, startTime, getHeuristicComparator(source, end));
     }
 
-    private Comparator<? super Point2D> getHeuristicComparator() {
-        return (a, b) -> {
-            return 0;
-        };
+    private Comparator<? super Point2D> getHeuristicComparator(Point2D source, Point2D end) {
+        return (a, b) -> Double.compare(getFitness(a, source, end), getFitness(b, source, end));
+    }
+
+    private double getFitness(Point2D current, Point2D source, Point2D end) {
+        return 0;
     }
 }
