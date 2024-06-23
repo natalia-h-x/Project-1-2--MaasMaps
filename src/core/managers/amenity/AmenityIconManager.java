28b8cf43d3aa62a6cf6dd658247348f8641d112b
alepskn <alexandraplishkin@gmail.com>
2024-06-23 13:10:43 +0200

    Create AmenityIconManager.java

---
+++ src/core/managers/amenity/AmenityIconManager.java
@@ -0,0 +1,32 @@
+package core.managers.amenity;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.swing.ImageIcon;
+
+import core.models.geojson.GeoData;
+
+public class AmenityIconManager {
+    private AmenityIconManager() {}
+
+    private static Map<String, ImageIcon> icons; 
+
+    public static void loadIcon() {
+        Map<String, List<GeoData>> geoData = AmenitySerializationManager.getGeoData();
+
+        for (Map.Entry<String, List<GeoData>> data: geoData.entrySet()) {
+            for (GeoData amenity : data.getValue()) {
+                icons.computeIfAbsent(data.getKey(), k -> amenity.getIcon());
+            }
+        }
+    }
+    
+
+    public static Map<String, ImageIcon> getAmenityIcons() {
+        if (icons == null)
+            loadIcon();
+
+        return icons;
+    }
+}
