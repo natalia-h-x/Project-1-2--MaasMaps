51f859e9738cc4cb1377f31ed920957ebe3a21e4
sian2005 <sianlodde@gmail.com>
2024-06-23 15:32:53 +0200

    Update AmenityStatisticsManager.java

--- src/core/managers/amenity/AmenityStatisticsManager.java
+++ src/core/managers/amenity/AmenityStatisticsManager.java
@@ -1,11 +1,21 @@
 package core.managers.amenity;
 
 public class AmenityStatisticsManager {
+    private AmenityStatisticsManager() {}
 
-    public static double getMaxAccessibilityMetric() {return Math.random();}
+    public static double getMaxAccessibilityMetric() {
+        return getAccessibilityMetric(1);
+    }
 
-    public static double getMinAccessibilityMetric() {return Math.random();}
+    public static double getMinAccessibilityMetric() {
+        return getAccessibilityMetric(0);
+    }
 
-    public static double getMedianAccessibilityMetric() {return Math.random();}
+    public static double getMedianAccessibilityMetric() {
+        return getAccessibilityMetric(0.5);
+    }
 
+    public static double getAccessibilityMetric(double percentage) {
+        return Math.random(); // Alexandra
+    }
 }
